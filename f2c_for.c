5ccf75f361b9596c5adc9ac103929aa125db682c
theDebonair <harshit4277448@live.com>
2022-07-06 12:17:59 +0530

    New programs added

--- f2c_for.c
+++ f2c_for.c
@@ -2,9 +2,9 @@
 
 void main() {
 	int fahr;
-	
-	// using for loop to print the reverse of fahr to celcius table
-	for(fahr = 300; fahr>=0; fahr -= 20) {
+
+	// program to print table of fahr to celcius conversion
+	for(fahr = 0; fahr <= 300; fahr += 20) {
 		printf("%3d°\t%6.2f°\n", fahr, (5.0/9.0)*(fahr-32.0));
 	}
 }
