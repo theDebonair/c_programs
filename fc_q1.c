d97f5bbc1754c740638f0a9bd02282551e48b7bb
theDebonair <harshit4277448@live.com>
2022-07-14 11:33:01 +0530

    Added new programs

---
+++ fc_q1.c
@@ -0,0 +1,10 @@
+// Exercise 1-6
+
+#include <stdio.h>
+
+void main() {
+	int c;
+
+	c = getchar() != EOF;
+	printf("%d", c);
+}
---
+++ fc_q2.c
@@ -0,0 +1,9 @@
+// Exercise 1-7
+
+#include <stdio.h>
+
+void main() {
+	int c = EOF;
+
+	printf("%d", c);
+}
